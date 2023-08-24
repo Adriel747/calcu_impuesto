ed3089fcf475d38c823a1154f035268e90b0b2f0
Adriel Fernandez <adriel.fernandez@ucb.edu.bo>
2023-08-24 10:16:59 -0400

    mostrar precio neto

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,18 @@
-import sumar from "./sumador";
+import multiplicar from "./multiplicador.js";
 
 const first = document.querySelector("#total");
-const form = document.querySelector("#totalizar-form");
+const second = document.querySelector("#precio");
+//const form = document.querySelector("#totalizar-form");
 const div = document.querySelector("#resultado-div");
+const button1 = document.querySelector("#totalizar");
 
-form.addEventListener("submit", (event) => {
+
+
+button1.addEventListener("click", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + firstNumber + "</p>";
+  div.innerHTML = "<p>" + "el precio neto es: " + firstNumber + "*" +secondNumber + " = " + multiplicar(firstNumber,secondNumber) +  "</p>";
 });
