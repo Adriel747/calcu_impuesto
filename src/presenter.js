f1f7f02dd60460929c9bd0c4568fa8427e43b37f
Adriel Fernandez <adriel.fernandez@ucb.edu.bo>
2023-08-29 00:44:24 -0400

    mostrar el precio con el personaje de descuento de 1000

--- src/presenter.js
+++ src/presenter.js
@@ -11,26 +11,21 @@ const button1 = document.querySelector("#totalizar");
 const des = document.querySelector("#descuento-div");
 var estado = document.getElementById("#estado");
 
-
-
-
 button1.addEventListener("click", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
+
   let total = multiplicar(firstNumber, secondNumber);
   let porcentajeDescuento  = descuento(total);
   let totalDescuento  = multiplicar(porcentajeDescuento,total);
   
 
   //div.innerHTML = "<p>" + "el precio neto es: " + firstNumber + "*" +secondNumber + " = " + multiplicar(firstNumber,secondNumber) +  "</p>";
-  let imp = impuesto(estado.value);
-  let totalimpuesto= multiplicar(imp,total).toFixed(2);
+  
 
-  precioNeto.innerHTML = "<p>El precio neto es: "+firstNumber+ "*"+ secondNumber +" = "+total+ "</p>";
-  des.innerHTML = "<p>El descuento es: "+multiplicar(porcentajeDescuento,100).toFixed(2)+"% = "+totalDescuento+"</p>";
-  impuestoDiv.innerHTML = "<p>El descuento para "+lugar.value+" es "+ multiplicar(imp,100).toFixed(2)+"%"
-  +" = " +totalImpuesto+"</p>"
- des.innerHTML = "<p>" + "el precio neto es: " + firstNumber + "*" +secondNumber + " = " + totalDescuento +  "</p>";
+  des.innerHTML= "<p>"+ "precio neto es"+firstNumber+"*"+secondNumber+"="+multiplicar(firstNumber,secondNumber)  +"</p>";
+  //des.innerHTML = "<p>" + "el descuento neto es: " + firstNumber + "*" +secondNumber + " = " + totalDescuento +  "</p>";
+  des.innerHTML = "<p>El descuento es: "+porcentajeDescuento+"% = "+totalDescuento+"</p>";
 });
